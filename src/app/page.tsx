2d71854598dc64790c506124af854b779369f3cc
angle-create <angle.create@proton.me>
2025-02-19 10:42:41 +0900

    fix: コンポーネントのスクロール設定を改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ import { Clock } from '@/components/dashboard/Clock'
 export default function DashboardPage() {
   return (
     <AppLayout>
-      <div className="flex flex-col lg:flex-row gap-6 h-[calc(100vh-4rem)] py-2">
+      <div className="flex flex-col lg:flex-row gap-6 h-[calc(100vh-5rem)] py-4">
         <div className="lg:w-1/2 flex flex-col gap-6">
           <div className="h-44">
             <Clock />
           </div>
-          <div className="flex-1 min-h-0 overflow-hidden">
+          <div className="flex-1 min-h-0 overflow-auto">
             <TodaySchedules />
           </div>
         </div>
-        <div className="lg:w-1/2 flex flex-col min-h-0 overflow-hidden">
+        <div className="lg:w-1/2 flex flex-col min-h-0 overflow-auto">
           <TodoList />
         </div>
       </div>
